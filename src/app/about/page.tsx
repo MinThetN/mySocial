f2429fe681f05ad7ca01a5ad34b46400af0f0bc8
MinThetN <minthetn15@gmail.com>
2025-05-14 18:14:59 +0700

    created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+    console.log("hello from about page")
+  return (
+    <div>
+      about page
+    </div>
+  )
+}
+
+export default AboutPage
