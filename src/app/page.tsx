90b7292c54347caccce89c9678e5ba1a59fcbf82
MinThetN <minthetn15@gmail.com>
2025-05-17 13:48:49 +0700

    completed auth and sync data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
+  
   return (
     <div className="m-4">
       <h1>Home page</h1>
