84d2f278c540be59e8dc288ed322507a047a1114
MinThetN <minthetn15@gmail.com>
2025-05-22 13:51:53 +0700

    completed profile page section

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,29 +1,39 @@
-import { getProfileByUsername } from '@/actions/profile.action'
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action'
 import { notFound } from 'next/navigation';
 import React from 'react'
+import ProfilePageClient from './ProfilePageClient';
 
-export async function generateMetadata({params} : {params: {username: string}}) {
-  const user = await getProfileByUsername(params.username);
+// export async function generateMetadata({params} : {params: {username: string}}) {
+//   const user = await getProfileByUsername(params.username);
   
-  if (!user) return null;
+//   if (!user) return null;
 
 
-  return {
-    title: `${user.name ?? user.username}`,
-    description: user.bio || `Check out ${user.username}'s profile.`,
-  };
-}
+//   return {
+//     title: `${user.name ?? user.username}`,
+//     description: user.bio || `Check out ${user.username}'s profile.`,
+//   };
+// }
 
-async function ProfilePage({params} : {params: {username: string}}) {
+async function ProfilePageServer({params} : {params: {username: string}}) {
   const user = await getProfileByUsername(params.username);
 
   if (!user) notFound();
 
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([ // server component
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ])
+
   return (
-    <div>
-      Profile page
-    </div>
+    <ProfilePageClient 
+      user = {user}
+      posts = {posts}
+      likedPosts = {likedPosts}
+      isFollowing = {isCurrentUserFollowing}
+    />
   )
 }
 
-export default ProfilePage
+export default ProfilePageServer
