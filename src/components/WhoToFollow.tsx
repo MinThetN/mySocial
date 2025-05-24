877a5781ae19f0c990852c53dd455ac1299ef2cd
MinThetN <minthetn15@gmail.com>
2025-05-24 13:11:46 +0700

    updated

--- src/components/WhoToFollow.tsx
+++ src/components/WhoToFollow.tsx
@@ -20,21 +20,22 @@ async function WhoToFollow() {
         <CardContent>
             <div className="space-y-4">
             {users.map((user) => (
-                <div key={user.id} className="flex gap-2 items-center justify-between ">
-                <div className="flex items-center gap-1">
-                    <Link href={`/profile/${user.username}`}>
-                    <Avatar>
-                        <AvatarImage src={user.image ?? "/avatar.png"} />
-                    </Avatar>
-                    </Link>
-                    <div className="text-xs">
-                    <Link href={`/profile/${user.username}`} className="font-medium cursor-pointer">
-                        {user.name}
-                    </Link>
-                    <p className="text-muted-foreground">@{user.username}</p>
-                    <p className="text-muted-foreground">{user._count.followers} followers</p>
+                <div key={user.id} className="flex gap-2 items-center justify-between p-2 
+                rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-800 transition ">
+                    <div className="flex items-center gap-1 min-w-0">
+                        <Link href={`/profile/${user.username}`} className='shrink-0'>
+                        <Avatar className='w-8 h-8'>
+                            <AvatarImage src={user.image ?? "/avatar.png"} />
+                        </Avatar>
+                        </Link>
+                        <div className="text-xs">
+                        <Link href={`/profile/${user.username}`} className="font-medium cursor-pointer">
+                            {user.name}
+                        </Link>
+                        <p className="text-muted-foreground max-w-[100px] flex-1">@{user.username}</p>
+                        <p className="text-muted-foreground">{user._count.followers} followers</p>
+                        </div>
                     </div>
-                </div>
                     <FollowButton userId={user.id} />
                 </div>
             ))}
